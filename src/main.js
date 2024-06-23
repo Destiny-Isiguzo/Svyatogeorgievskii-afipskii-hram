40eb6f8d78618bb308875a1d9c4915e8a49c340f
Des19 <petrovichalexander411@gmail.com>
2024-06-23 20:30:42 +0300

    "Updated index.html, main.js, and style files with various changes to HTML structure, and JavaScript logic"

--- src/main.js
+++ src/main.js
@@ -3,24 +3,33 @@ const HEADER_ACTIVE_SCROLL_Y = 40;
 const SCROLL_UP_BTN_SHOW_SCROLL_Y = 1500;
 
 
+// Cache DOM elements
+const header = document.querySelector('.header');
+const main = document.querySelector('.main');
+const navMenu = document.getElementById('nav-menu-container');
+const navMenuOverlay = document.getElementById('nav-menu-container-overlay');
+const navBtns = document.querySelectorAll('#open-nav-btn, #close-nav-btn');
+const preloader = document.getElementById('preloader');
+const scrollUpBtn = document.getElementById('scrollup-btn');
+const scrollY = window.scrollY;
+const scheduleCards = document.querySelectorAll('.schedule-card');
+const date = new Date();
+const dayOfWeek = date.toLocaleString('en-US', { weekday: 'long' });
+const year = document.getElementById('year');
+
+
 // Extracted functions
 
 function addFadeOutClass(element) {
-  element.classList.toggle('fade-out', true);
+  element.classList.add('fade-out');
 }
 
 function showHeaderAndMain() {
-  const header = document.querySelector('.header');
-  const main = document.querySelector('.main');
   header.style.opacity = '1';
   main.style.opacity = '1';
 }
 
 function toggleNavMenu() {
-  const navMenu = document.getElementById('nav-menu-container');
-  const navMenuOverlay = document.getElementById('nav-menu-container-overlay');
-  const navBtns = document.querySelectorAll('#open-nav-btn, #close-nav-btn');
-
   navBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
       navMenu.classList.toggle('show-menu');
@@ -37,7 +46,6 @@ function toggleNavMenu() {
 }
 
 function handleScroll() {
-  const scrollY = window.scrollY;
   // document.getElementById('header').classList.toggle('header-active', scrollY > HEADER_ACTIVE_SCROLL_Y);
   document.getElementById('scrollup-btn').classList.toggle('scrollup-btn-show', scrollY > SCROLL_UP_BTN_SHOW_SCROLL_Y);
 }
@@ -55,7 +63,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 window.addEventListener('load', () => {
-  const preloader = document.getElementById('preloader');
   addFadeOutClass(preloader);
   showHeaderAndMain();
 })
@@ -64,17 +71,13 @@ window.addEventListener('scroll', handleScroll);
 document.getElementById('scrollup-btn').addEventListener('click', scrollToTop);
 
 
-const scheduleCards = document.querySelectorAll('.schedule-card');
-const date = new Date();
-const dayOfWeek = date.toLocaleString('en-US', { weekday: 'long' });
+// scheduleCards.forEach((card) => {
+//   const dayElement = card.querySelector('.schedule-card-day');
 
-scheduleCards.forEach((card) => {
-  const dayElement = card.querySelector('.schedule-card-day');
-
-  if (dayElement.innerText === dayOfWeek) {
-    card.classList.add('schedule-card-active');
-  }
-});
+//   if (dayElement.innerText === dayOfWeek) {
+//     card.classList.add('schedule-card-active');
+//   }
+// });
 
 
 // Scroll reveal animation
@@ -91,5 +94,4 @@ sr.reveal(`.schedule-card`, {
 
 
 // Footer year
-const year = document.getElementById('year');
 year.innerText = new Date().getFullYear();
